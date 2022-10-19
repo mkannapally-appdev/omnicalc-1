f236ee67c4ac1d34cea2f9fa4654786f5fa03d45
mkannapally <akannapa@chicagobooth.edu>
2022-10-19 22:44:35 +0000

    payment fully working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,14 +33,24 @@ class ApplicationController < ActionController::Base
   end
 
   def payment_results
-    @apr = params.fetch("user_apr").to_f
-    @r = @apr / 100
 
     @num_of_years = params.fetch("user_years").to_f
-    @principal = params.fetch("user_pay")
+    @num_of_periods = @num_of_years * 12
 
-    @numerator = @principal * @r
-    @payment = 1 - (1+@r)**@num_of_years
+    @apr = params.fetch("user_apr").to_f
+    @r = @apr / 1200
+
+    @apr = @apr.to_s(:percentage , {:precision => 4})
+
+    @principal_i = params.fetch("user_pay").to_f
+    @principal = @principal_i.to_s(:currency)
+
+    @numerator = @principal_i * @r
+    #@denominator = (1 + @r)**(1 / @num_of_years)
+    @denominator = (1 + @r)**( - @num_of_periods )
+
+    @payment = (@numerator/ (1 - @denominator)).to_s(:currency)
+    #@payment = @numerator
 
     render({:template => "calc_templates/payment_results.html.erb"})
 
